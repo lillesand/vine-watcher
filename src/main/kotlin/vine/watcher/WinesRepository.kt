07f3071c7f83a4688e9ceb90adec9e5ed8e62170
Jøran Vagnby Lillesand <lillesand@gmail.com>
2020-04-03 15:15:47 +0200

    Fixed s3 bucket name. getenv, not getProperty!

--- src/main/kotlin/vine/watcher/WinesRepository.kt
+++ src/main/kotlin/vine/watcher/WinesRepository.kt
@@ -7,7 +7,7 @@ import com.beust.klaxon.Klaxon
 
 class WinesRepository {
 
-    private val environment = System.getProperty("STAGE") ?: "local"
+    private val environment = System.getenv("STAGE") ?: "local"
     private val bucketName = "vinewatcher-$environment"
     private val winesFileName = "wines.json"
 
