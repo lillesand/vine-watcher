23e16d94a9d9766740c39f7ec48d8312ef1a6fa9
Jøran Vagnby Lillesand <lillesand@gmail.com>
2020-04-10 15:12:06 +0200

    Remove old hardcoded vines

--- src/main/kotlin/vine/watcher/App.kt
+++ src/main/kotlin/vine/watcher/App.kt
@@ -7,18 +7,6 @@ class App(private val vmpClient: VMPClient = VMPClient(),
           private val winesRepository: WinesRepository = WinesRepository()) {
 
     private val maxTravelKms = 10
-    private val wines = mapOf(
-            "8278501" to "Zind-Humbrecht Riesling Brand Grand Cru 2017",
-            "10802701" to "Faiveley Mercurey Premier Cru Clos du Roy 2017",
-            "10802801" to "Faiveley Mercurey La Framboisère 2017",
-            "11416405" to "Penfolds St. Henri Shiraz 2006",
-            "5089801" to "Viña Tondonia Gran Reserva Rosado 2009",
-            "7879101" to "Dom. de la Côte Memorious Pinot Noir 2017",
-            "7879201" to "Dom. de la Côte Bloom's Field Pinot Noir 2017",
-            "7879001" to "Dom. de la Côte Sta. Rita Hills Pinot Noir 2016",
-            "7879301" to "La Côte Pinot Noir 2017",
-            "5089801" to "Viña Tondonia Gran Reserva Rosado 2009"
-    )
 
     fun postStatus() {
         val previousWineStatus = winesRepository.previousWineStatus()
